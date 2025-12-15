b16a90a650405fe773174f4ac244e5eabaed3bb8
Nizar Daffa <izardev@gmail.com>
2025-12-15 21:42:27 +0700

    feat: add additional pengguna users to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -33,6 +33,66 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('12345678'),
                 'role' => 'keuangan'
             ],
+            [
+                'name' => 'Budi Santoso',
+                'email' => 'budi.santoso@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Siti Nurhaliza',
+                'email' => 'siti.nurhaliza@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Ahmad Rizki',
+                'email' => 'ahmad.rizki@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Dewi Lestari',
+                'email' => 'dewi.lestari@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Eko Prasetyo',
+                'email' => 'eko.prasetyo@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Rina Wati',
+                'email' => 'rina.wati@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Joko Widodo',
+                'email' => 'joko.widodo@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Maya Sari',
+                'email' => 'maya.sari@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Andi Wijaya',
+                'email' => 'andi.wijaya@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
+            [
+                'name' => 'Putri Ayu',
+                'email' => 'putri.ayu@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'pengguna'
+            ],
         ];
 
         foreach ($users as $user) {
